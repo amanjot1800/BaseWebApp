e88b6ca5c70a253abdcce1a8e9a48961b497ca3f
Amanjot Singh <amanjot1800@gmail.com>
2020-09-04 16:29:45 -0400

    Timing check

--- project/js/main.js
+++ project/js/main.js
@@ -5,6 +5,7 @@ function showPicture(){
   $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
   $("p").html("High five! You're building your first web app!");
   $("#new").html("This is really great that i just have to make changes in editor and everything is live.")
+  $("#new2").html("This is for the timing check...")
 
 
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
